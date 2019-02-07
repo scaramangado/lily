2bda1124e00f0c209895d70383b830df9177112d
scaramangado <scaramangado@gmail.com>
2019-02-07 21:06:27 +0100

    Customized Default Banner.

--- src/main/java/de/pieroavola/lily/application/Lily.java
+++ src/main/java/de/pieroavola/lily/application/Lily.java
@@ -2,6 +2,9 @@ package de.pieroavola.lily.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 /**
  * Class used to bootstrap a Spring Boot based Lily application.
@@ -20,8 +23,29 @@ public class Lily {
      * @param args the command line arguments.
      * @return the Spring Boot application context.
      */
+    @SuppressWarnings("UnusedReturnValue") // Public method of framework
     public static ConfigurableApplicationContext run(Class<?> primarySource, String... args) {
 
-        return SpringApplication.run(primarySource, args);
+        SpringApplication application = new SpringApplication(primarySource);
+        application.setBanner(Lily::printBanner);
+
+        return application.run(args);
+    }
+
+    @SuppressWarnings({"squid:S1174", "unused"}) // Used as lambda.
+    private static void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+        out.println(
+                " ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄       ▄         ▄ \n" +
+                "▐░▌          ▐░░░░░░░░░░░▌▐░▌     ▐░▌       ▐░▌\n" +
+                "▐░▌           ▀▀▀▀█░█▀▀▀▀ ▐░▌     ▐░▌       ▐░▌\n" +
+                "▐░▌               ▐░▌     ▐░▌     ▐░▌       ▐░▌\n" +
+                "▐░▌               ▐░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌\n" +
+                "▐░▌               ▐░▌     ▐░▌     ▐░░░░░░░░░░░▌\n" +
+                "▐░▌               ▐░▌     ▐░▌      ▀▀▀▀█░█▀▀▀▀ \n" +
+                "▐░▌               ▐░▌     ▐░▌          ▐░▌     \n" +
+                "▐░█▄▄▄▄▄▄▄▄▄  ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌     \n" +
+                "▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌     \n" +
+                " ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀      \n" +
+                "                                                 ");
     }
 }
