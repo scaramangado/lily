9d4f03416665b80db8a52eea1bd2def9ff762d6b
scaramangado <scaramangado@gmail.com>
2019-02-08 23:13:54 +0100

    Moved default banner to text file.

--- src/main/java/de/pieroavola/lily/application/Lily.java
+++ src/main/java/de/pieroavola/lily/application/Lily.java
@@ -1,19 +1,25 @@
 package de.pieroavola.lily.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
+import java.io.IOException;
 import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Class used to bootstrap a Spring Boot based Lily application.
  */
 public class Lily {
 
-    /**
-     * Private Constructor.
-     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(Lily.class);
+
     private Lily() { }
 
     /**
@@ -26,7 +32,7 @@ public class Lily {
     @SuppressWarnings("UnusedReturnValue") // Public method of framework
     public static ConfigurableApplicationContext run(Class<?> primarySource, String... args) {
 
-        SpringApplication application = new SpringApplication(primarySource);
+        final SpringApplication application = new SpringApplication(primarySource);
         application.setBanner(Lily::printBanner);
 
         return application.run(args);
@@ -34,18 +40,20 @@ public class Lily {
 
     @SuppressWarnings({"squid:S1174", "unused"}) // Used as lambda.
     private static void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
-        out.println(
-                " ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄       ▄         ▄ \n" +
-                "▐░▌          ▐░░░░░░░░░░░▌▐░▌     ▐░▌       ▐░▌\n" +
-                "▐░▌           ▀▀▀▀█░█▀▀▀▀ ▐░▌     ▐░▌       ▐░▌\n" +
-                "▐░▌               ▐░▌     ▐░▌     ▐░▌       ▐░▌\n" +
-                "▐░▌               ▐░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌\n" +
-                "▐░▌               ▐░▌     ▐░▌     ▐░░░░░░░░░░░▌\n" +
-                "▐░▌               ▐░▌     ▐░▌      ▀▀▀▀█░█▀▀▀▀ \n" +
-                "▐░▌               ▐░▌     ▐░▌          ▐░▌     \n" +
-                "▐░█▄▄▄▄▄▄▄▄▄  ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌     \n" +
-                "▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌     \n" +
-                " ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀      \n" +
-                "                                                 ");
+
+        try {
+            final URL bannerUrl = Lily.class.getClassLoader().getResource("default_banner.txt");
+
+            if (bannerUrl == null) {
+                throw new NullPointerException("Cannot load resource.");
+            }
+
+            final String banner = new String(Files
+                    .readAllBytes(Paths.get(bannerUrl.toURI())));
+
+            out.println(banner);
+        } catch (URISyntaxException | IOException e) {
+            LOGGER.error("Could not load default banner from text file.", e);
+        }
     }
 }
