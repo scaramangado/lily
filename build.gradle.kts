def7751dbb70b222dfa65b30ed2e45a21aa48276
scaramangado <scaramangado@gmail.com>
2020-07-19 20:39:21 +0200

    Upgraade Gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,31 +15,32 @@ repositories {
 
 dependencies {
 
-  api("org.springframework.boot:spring-boot-starter:2.1.8.RELEASE")
-  api("net.dv8tion:JDA:4.0.0_46") {
+  api("org.springframework.boot:spring-boot-starter:2.3.1.RELEASE")
+  api("net.dv8tion:JDA:4.2.0_180") {
     exclude(module = "opus-java")
   }
 
   implementation("org.reflections:reflections:0.9.11")
 
-  lombok("1.18.8")
+  lombok("1.18.12")
 
-  val jUnitVersion = "5.5.2"
+  val jUnitVersion = "5.6.2"
   testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
   testImplementation("org.junit.jupiter:junit-jupiter-params:$jUnitVersion")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
 
-  testImplementation("org.springframework.boot:spring-boot-starter-test:2.1.8.RELEASE") {
+  testImplementation("org.springframework.boot:spring-boot-starter-test:2.3.1.RELEASE") {
     exclude(module = "junit")
     exclude(module = "hamcrest-library")
     exclude(module = "hamcrest-core")
     exclude(module = "json-path")
     exclude(module = "jsonassert")
     exclude(module = "xmlunit-core")
+    exclude(group = "org.junit.vintage")
   }
 
-  testImplementation("org.assertj:assertj-core:3.13.2")
-  testImplementation("org.awaitility:awaitility:4.0.1")
+  testImplementation("org.assertj:assertj-core:3.16.1")
+  testImplementation("org.awaitility:awaitility:4.0.3")
 }
 
 tasks.withType(Test::class) {
@@ -102,5 +103,5 @@ fun DependencyHandler.lombok(version: Any) {
 }
 
 tasks.withType(Wrapper::class) {
-  gradleVersion = "5.6.2"
+  gradleVersion = "6.5.1"
 }
